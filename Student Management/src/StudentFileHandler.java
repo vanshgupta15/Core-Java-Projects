55d24aa82563dd3426bbc3a95ea16c56fb27e183
Vansh Gupta <vanshsja1512@gmail.com>
2025-06-04 13:26:57 +0530

    Getallstudents
    Function to get all students has been created and working.. Work in progress for student details

--- Student Management/src/StudentFileHandler.java
+++ Student Management/src/StudentFileHandler.java
@@ -26,6 +26,8 @@ public class StudentFileHandler
             FileInputStream fileIn = new FileInputStream("Data/"+id+".txt");
             ObjectInputStream in = new ObjectInputStream(fileIn);
             System.out.println(in.readObject());
+            in.close();
+            fileIn.close();
         }
         catch(Exception e)
         {
@@ -35,6 +37,18 @@ public class StudentFileHandler
 
     public void studentDisplay()
     {
-        
+        try
+        {
+            File file= new File("C:\\Vansh_codings\\codecoach\\Core-java-projects\\Student Management\\Data");
+            String [] paths=file.list();
+            for(String path:paths)
+            {
+                System.out.println(path);
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
     }
 }
