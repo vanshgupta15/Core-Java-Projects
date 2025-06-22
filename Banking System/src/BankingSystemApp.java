62208965da7accbfaa7a6af933f7dda71a439691
Vansh Gupta <vanshsja1512@gmail.com>
2025-06-22 13:28:29 +0530

    Update BankingSystemApp.java

--- Banking System/src/BankingSystemApp.java
+++ Banking System/src/BankingSystemApp.java
@@ -1,53 +1,81 @@
 import java.util.Scanner;
-public class BankingSystemApp 
-{
-    public static void main(String[] args) 
-    {
-        Scanner sc= new Scanner(System.in);
-        System.out.println("===== Java Bank Menu =====");
-        System.out.println("1. Create New Account");
-        System.out.println("2. Deposit");
-        System.out.println("3. Withdraw");
-        System.out.println("4. Display Account Details");
-        System.out.println("5. Exit");
-        System.out.print("Enter your choice: ");
-        int choice=sc.nextInt();
-        switch(choice)
-        {
-            case 1:
-            {
-                System.out.println("Enter account holder name: ");
-                String name= sc.nextLine();
-                System.out.println("Enter account number: ");
-                String accountNumber= sc.nextLine();
-                System.out.println("Enter your initial balance: ");
-                double balance= sc.nextDouble();
-                System.out.println("Type of account:-\n1.Saving Account\n2.Current Account");
-                int t= sc.nextInt();
-                switch(t)
-                {
-                    case 1:
-                    {
-                        SavingAccount account= new SavingAccount(accountNumber, name, balance);
-                        break;
+
+public class BankingSystemApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean check = true;
+
+        while (check) {
+            System.out.println("\n===== Java Bank Menu =====");
+            System.out.println("1. Create New Account");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Display Account Details");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = sc.nextInt();
+            sc.nextLine(); 
+
+            switch (choice) {
+                case 1: {
+                    System.out.print("Enter account holder name: ");
+                    String name = sc.nextLine();
+                    System.out.print("Enter account number: ");
+                    String accountNumber = sc.nextLine();
+                    System.out.print("Enter your initial balance: ");
+                    double balance = sc.nextDouble();
+                    sc.nextLine(); 
+                    System.out.println("Type of account:-\n1. Saving Account\n2. Current Account");
+                    int t = sc.nextInt();
+                    sc.nextLine();
+                    switch (t) {
+                        case 1:
+                            SavingAccount acc1 = new SavingAccount(accountNumber, name, balance);
+                            break;
+                        case 2:
+                            CurrentAccount acc2 = new CurrentAccount(accountNumber, name, balance);
+                            break;
                     }
-                    case 2:
-                    {
-                        CurrentAccount account= new CurrentAccount(accountNumber, name, balance);
-                        break;
-                    }
+                    break;
                 }
-                break;
-            }
-            case 2:
-            {
-                System.out.println("Enter account number: ");
-                String accountNumber= sc.nextLine();
-                Account account = null;
-                account.findAccount(accountNumber);
-                System.out.println("Enter the amount to deposit: ");
-                double deposit= sc.nextDouble();
-                account.deposits(deposit);
+                case 2: {
+                    System.out.print("Enter account number: ");
+                    String accountNumber = sc.nextLine();
+                    Account account = null; 
+                    account=account.findAccount(accountNumber);
+                    System.out.print("Enter the amount to deposit: ");
+                    double deposit = sc.nextDouble();
+                    sc.nextLine(); 
+                    account.deposits(deposit);
+                    break;
+                }
+                case 3: {
+                    System.out.print("Enter account number: ");
+                    String accountNumber = sc.nextLine();
+                    Account account = null; 
+                    account=account.findAccount(accountNumber);
+                    System.out.print("Enter the amount to withdraw: ");
+                    double withdraw = sc.nextDouble();
+                    sc.nextLine();
+                    account.withdraw(withdraw);
+                    break;
+                }
+                case 4: {
+                    System.out.print("Enter account number: ");
+                    String accountNumber = sc.nextLine();
+                    Account account = null ; 
+                    account=account.findAccount(accountNumber);
+                    account.displayAccountDetails();
+                    break;
+                }
+                case 5: {
+                    System.out.println("Thank you for using Java Bank. Goodbye!");
+                    check = false;
+                    break;
+                }
+                default:
+                    System.out.println("Invalid choice. Please select between 1 to 5.");
             }
         }
     }
